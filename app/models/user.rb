b9b772b597d41543b049dea707ee40e61f3428f6
ebfly <74950051+ebfly@users.noreply.github.com>
2020-12-20 14:20:03 +0900

    新規登録・ログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  
   validates :nickname,presence: true
   validates :email,   presence: true
   validates :encrypted_password, presence: true
@@ -12,5 +12,5 @@ class User < ApplicationRecord
   validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
   validates :birth_day, presence: true
-
+ 
 end
