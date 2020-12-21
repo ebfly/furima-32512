70e030afbc9a955e07061207288c1adff58b1797
ebfly <74950051+ebfly@users.noreply.github.com>
2020-12-21 23:03:24 +0900

    ユーザー新規等のテストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  validates :nickname,presence: true
-  validates :email,   presence: true
-  validates :encrypted_password, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+
+  validates :nickname, presence: true, length: { maximum: 40 }
+  validates :email, presence: true
+  # 英数字を入力させるオプション
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  # 全角ひらがな、カタカナ、漢字で入力させるオプション
+  validates :last_name, :first_name, presence: true,
+                                     format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
+  validates :last_name_kana, :first_name_kana, presence: true,
+                                               format: { with: /\A[\p{katakana}ー－&&[^ -~｡-ﾟ]]+\z/ }
   validates :birth_day, presence: true
- 
 end
