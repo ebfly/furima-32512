d72e0847dd903f14af24488df60f9a7bf5e3ca94
ebfly <74950051+ebfly@users.noreply.github.com>
2021-01-04 15:37:53 +0900

    コード修正

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -4,7 +4,7 @@ class AddressesController < ApplicationController
 
     def index
       @order_address = OrderAddress.new
-      if current_user == @item.user_id
+      if current_user == @item.user
         redirect_to root_path
       end
     end
